125adc4903713d32f14b00c5608fdc47a35ddc7a
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-01-13 21:57:15 +0000

    UPDATE: Fixed the package.json and versioning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  get-npm-version - Returns the NPM version installed in your environment
- *  @version: v1.0.9
+ *  @version: v1.1.0
  *  @link: https://github.com/tutyamxx/get-npm-version
  *  @license: MIT
  **/
